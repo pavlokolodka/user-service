2307963d1bbdf43f742ed7494f3ea969ffe16ec3
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-06-17 14:53:41 +0300

    Add user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './user/entity/user.entity';
 import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { RolesModule } from './roles/roles.module';
+import { Role } from './roles/entity/roles.entity';
+import { UserRoles } from './roles/entity/user-roles.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,11 +21,14 @@ import { UserController } from './user/user.controller';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [],
+      entities: [User, Role, UserRoles],
       synchronize: true,
-  })
+  }),
+    UserModule,
+    RolesModule,
+    AuthModule
   ],
-  controllers: [AppController, UserController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,34 @@
+import { UserRoles } from "src/roles/entity/user-roles.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+interface IUserCreate {
+  email: string,
+  password: string
+}
+
+@Entity({name: 'users'})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({unique: true})
+  email: string
+ 
+  @Column()
+  password: string
+
+  @Column({name: 'is_banned', default: false})
+  banned: boolean
+
+  @Column({name: 'ban_reason', nullable: true})
+  banReason: string
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @OneToMany(() => UserRoles, userRoles => userRoles.user)
+  public userRoles!: UserRoles[];
+}
