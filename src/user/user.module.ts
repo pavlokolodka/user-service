b82b433888e82dcd5cfbe8c6cbd362d2b0d0be96
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-06-17 16:36:05 +0300

    Update module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -9,6 +9,7 @@ import { UserService } from './user.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User, UserRoles]), RolesModule],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService]
 })
 export class UserModule {}
