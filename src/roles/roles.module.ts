c46a65ab1a39363bc063b9321f5dadd37bf6ea9d
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-06-20 13:19:18 +0300

    Add to imports auth module

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Role } from './entity/roles.entity';
 import { UserRoles } from './entity/user-roles.entity';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role, UserRoles])],
+  imports: [TypeOrmModule.forFeature([Role, UserRoles]), AuthModule],
   controllers: [RolesController],
   providers: [RolesService],
   exports: [RolesService]
