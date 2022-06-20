654cb4247b40f75a7c83654de432cdf8f1efa32b
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-06-20 13:11:42 +0300

    Add validators

--- src/user/dto/ban-user.dto.ts
+++ src/user/dto/ban-user.dto.ts
@@ -1,4 +1,8 @@
+import { IsNumber, IsString } from "class-validator";
+
 export class BanUserDto {
+  @IsNumber({}, {message: 'Must be a number'})
   readonly userId: number;
+  @IsString({message: 'Must be a string'})
   readonly banReason: string;
 }
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsString, Length } from "class-validator";
+
 export class CreateUserDto {
+  @IsString({message: 'Must be a string'})
+  @IsEmail({}, { message: 'Invalid email message' })
   readonly email: string;
+  @IsString({message: 'Must be a string'})
+  @Length(4, 18, {message: 'Not less than 4 and not more than 18'})
   readonly password: string;
 }
