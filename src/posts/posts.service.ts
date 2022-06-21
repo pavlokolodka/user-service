c28d7d0e38f976328798dec02050d95e9d7112b1
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-06-21 09:32:25 +0300

    Update post functionality

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FilesService } from 'src/files/files.service';
+import { Repository } from 'typeorm';
+import { CreatePostDto } from './dto/create-post.dto';
+import { Post } from './entity/posts.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+
+  constructor(
+    @InjectRepository(Post) private postRepository: Repository<Post>,
+    private fileService: FilesService
+    ) {}
+  
+  async create(dto: CreatePostDto, image: any) {
+    const fileName = await this.fileService.createFile(image);
+    const post = this.postRepository.create({...dto, image: fileName});
+    await this.postRepository.save(post);
+    return post;
+  }
+}
