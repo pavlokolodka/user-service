2564a2d900d6002bbc98cd1fc95146e8ccafd283
Pavlo Kolodka <pavelkolodka.dev@gmail.com>
2022-06-21 09:06:53 +0300

    Add file functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './user/entity/user.entity';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { RolesModule } from './roles/roles.module';
 import { Role } from './roles/entity/roles.entity';
 import { UserRoles } from './roles/entity/user-roles.entity';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { Post } from './posts/entity/posts.entity';
+import { FilesModule } from './files/files.module';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
     ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -21,12 +28,14 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      entities: [User, Role, UserRoles],
+      entities: [User, Role, UserRoles, Post],
       synchronize: true,
   }),
     UserModule,
     RolesModule,
-    AuthModule
+    AuthModule,
+    PostsModule,
+    FilesModule
   ],
   controllers: [AppController],
   providers: [AppService],
